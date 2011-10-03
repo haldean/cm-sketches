ddf132476d5f168be8a1edfe34a7476efb48aa97
Will Brown <will.h.brown@gmail.com>
2011-10-03 00:59:56 -0400

    Added a tiny song in C

---
+++ tones/obfusc.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+void main(){int t;for(t=0;;t++){if(t>>20)return;putchar((t>>18==0?(((2*t)|((t>>10)*t))&(t>>8))|(t>>15?4*t:0)|(t>>17?(t>>4):0):(t>>19==0?(t*(t>>8)|(t>>13)|t*t):2*t|3*t|4*t)));}}
